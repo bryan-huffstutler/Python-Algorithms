702639f39b9f87a98914c0b7460e0b192107eb5a
bryan-huffstutler <bryan.huffstutler@bryanuniversity.edu>
2022-01-09 16:14:38 -0600

    shuffle.py

--- wk6/shuffle.py
+++ wk6/shuffle.py
@@ -4,11 +4,13 @@ def shuffle (arr) :
   #Time complexity of this algorithm is O(n)
   #because it has to iterate through the list
   for i in range(len(arr)-1, 0, -1):
-    #creates a random between 0, and i+1 (Due to 0 Index of list)
-    x=random.randint(0, i+1)
+    #creates a random between 0, and i-1
+    x=random.randint(0, i-1)
     #sets a random index to equal the current index of for loop
     arr[i], arr[x] = arr[x], arr[i]
   return arr
 
 input = [7, 20, 26, 31, 40, 51, 55, 63, 74, 81]
 print(shuffle(input))
+
+# Would like to see an example of a recursive shuffle function
